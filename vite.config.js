102a070e1885943998acb2be8bedcc3040f5931d
Almaz's Zefirka <almazbek.sheraliev321@gmail.com>
2025-04-29 17:42:41 +0600

    feat(deployment): add gh-pages deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-   plugins: [react()],
-   esbuild: {
-      include: /\.js$/,
-      exclude: [],
-      loader: 'jsx',
-   },
+export default defineConfig(({ mode }) => {
+   return {
+      plugins: [react()],
+      esbuild: {
+         include: /\.js$/,
+         exclude: [],
+         loader: 'jsx',
+      },
+      base: mode === 'gh-pages' ? '/Flexbox-Froggy-clone/' : '/',
+   }
 })
