5ecf23eb56c8783ed5f5f864813eb800869b6545
Almaz's Zefirka <almazbek.sheraliev321@gmail.com>
2025-04-29 17:37:00 +0600

    refactor(setup): fix cve issues & migrate to vite

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-
-
-import View from "./components/View/View";
-import Sidebar from "./components/Sidebar/Sidebar";
-
+import View from './components/View/View'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
+   const styles = {
+      display: 'flex',
+      minWidth: '600px',
+      margin: '0',
+      backgroundColor: '#43A047',
+      color: 'white',
+   }
 
-  const styles = {
-    display: 'flex',
-    minWidth: '600px',
-    margin: '0',
-    backgroundColor: '#43A047',
-    color: 'white'
+   return (
+      <div style={styles}>
+         <Sidebar />
+         <View />
+      </div>
+   )
 }
 
-
-  return (
-  <div style={styles}>
-    <Sidebar />
-    <View />
-  </div>
-    );
-}
-
-export default App;
+export default App
